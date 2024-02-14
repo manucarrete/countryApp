8eccb32fbd423efceb8133242dc6fe86903231a9
manucarrete <manuelcarrete.23@campuscamara.es>
2024-02-14 18:36:08 +0100

    First commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Routes } from '@angular/router';
+import { HomePageComponent } from './shared/pages/home-page/home-page.component';
+import { ContactPageComponent } from './shared/pages/contact-page/contact-page.component';
+import { AboutPageComponent } from './shared/pages/about-page/about-page.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+    { path: 'home', component: HomePageComponent },
+    { path: 'about', component: AboutPageComponent },
+    { path: 'contact', component: ContactPageComponent },
+    { path: '', redirectTo: 'home', pathMatch: 'full'},
+    { path: 'countries', 
+        loadChildren: () => import('./countries/country.routes').then(m =>
+            m.routes)
+    },
+];
